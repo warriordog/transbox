841bf5ff81f3eb4c844b9742b962c1314c41917c
Marie <marie@kaifa.ch>
2023-07-08 14:17:43 +0200

    Update soapbox.ts

--- app/soapbox/types/soapbox.ts
+++ app/soapbox/types/soapbox.ts
@@ -1,5 +1,6 @@
 import {
   PromoPanelItemRecord,
+  RulesPanelItemRecord,
   FooterItemRecord,
   CryptoAddressRecord,
   SoapboxConfigRecord,
@@ -8,6 +9,7 @@ import {
 type Me = string | null | false | undefined;
 
 type PromoPanelItem = ReturnType<typeof PromoPanelItemRecord>;
+type RulesPanelItem = ReturnType<typeof RulesPanelItemRecord>;
 type FooterItem = ReturnType<typeof FooterItemRecord>;
 type CryptoAddress = ReturnType<typeof CryptoAddressRecord>;
 type SoapboxConfig = ReturnType<typeof SoapboxConfigRecord>;
@@ -15,6 +17,7 @@ type SoapboxConfig = ReturnType<typeof SoapboxConfigRecord>;
 export {
   Me,
   PromoPanelItem,
+  RulesPanelItem,
   FooterItem,
   CryptoAddress,
   SoapboxConfig,
