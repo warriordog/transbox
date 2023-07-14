7bdbbe4c1e3b1f6866ad9feadbfd90c37bc21199
Mar0xy <marie@kaifa.ch>
2023-07-14 06:08:58 +0200

    upd(utils/listenbrainz): add listenbrainz play url

--- app/soapbox/utils/listenbrainz.ts
+++ app/soapbox/utils/listenbrainz.ts
@@ -6,8 +6,9 @@ const getLData = (LBUser: string) => {
       const img: string = data.data.payload.listens[0].track_metadata.mbid_mapping && data.data.payload.listens[0].track_metadata.mbid_mapping.caa_release_mbid ? `https://coverartarchive.org/release/${data.data.payload.listens[0].track_metadata.mbid_mapping.caa_release_mbid}/front-250` : 'https://transfem.space/plugins/listenbrainz/images/cover-art-placeholder.png';
       const artist: string = data.data.payload.listens[0].track_metadata.artist_name;
       const lastlisten: string = data.data.payload.latest_listen_ts;
-      const id: string = data.data.payload.listens[0].track_metadata.mbid_mapping && data.data.payload.listens[0].track_metadata.mbid_mapping.recording_mbid ? `https://musicbrainz.org/recording/${data.data.payload.listens[0].track_metadata.mbid_mapping.recording_mbid}` : '#';
-      return [title, img, artist, lastlisten, id];
+      const musicbrainzurl: string = data.data.payload.listens[0].track_metadata.mbid_mapping && data.data.payload.listens[0].track_metadata.mbid_mapping.recording_mbid ? `https://musicbrainz.org/recording/${data.data.payload.listens[0].track_metadata.mbid_mapping.recording_mbid}` : '#';
+      const listenbrainzurl: string = data.data.payload.listens[0].track_metadata.mbid_mapping && data.data.payload.listens[0].track_metadata.mbid_mapping.recording_mbid ? `https://listenbrainz.org/player?recording_mbids=${data.data.payload.listens[0].track_metadata.mbid_mapping.recording_mbid}` : '#';
+      return [title, img, artist, lastlisten, musicbrainzurl, listenbrainzurl];
     });
 };
 
